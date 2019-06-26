dce56e33b162f0e2de1fd82af044a846409ccdc3
NakagamiYuta <guguru0014@gmail.com>
2019-06-26 16:11:44 +0900

    [add] test1で行うテストを作成

--- test1.py
+++ test1.py
@@ -2,10 +2,21 @@ from addition import *
 from subtraction import *
 from multiplication import *
 from division import *
+from testfunc import *
 
 
 def main():
-    pass
+    add_result = addition(10, 20)
+    test_equal("Addition", add_result, 30)
+
+    sub_result = subtraction(60, 20)
+    test_equal("Subbtraction", sub_result, 40)
+
+    mul_result = multiplication(40, 50)
+    test_equal("Multiplication", 2000)
+
+    div_result = division(60, 2)
+    test_equal("Division", 30)
 
 
 if __name__ == '__main__':

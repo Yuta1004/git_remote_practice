d9ecc116a22db839ee99367888e02d5849bd26e6
NakagamiYuta <guguru0014@gmail.com>
2019-06-26 16:20:16 +0900

    [add] テスト成功時にメッセージを表示するようにした

--- testfunc.py
+++ testfunc.py
@@ -4,15 +4,18 @@ class TestFailedException(Exception):
 
 def test_equal(name, comp_a, comp_b):
     if comp_a != comp_b:
-        raise TestFailedError(name)
+        raise TestFailedException(name)
+    print(name + " : Success!")
 
 
 def test_not_equal(name, comp_a, comp_b):
     if comp_a == comp_b:
-        raise TestFailedError(name)
+        raise TestFailedException(name)
+    print(name + " : Success!")
 
 
 def test(name, bool_val):
     if not bool_val:
-        raise TestFailedError(name)
+        raise TestFailedException(name)
+    print(name + " : Success!")
 

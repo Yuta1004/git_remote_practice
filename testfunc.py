b9f53beb5be1687df8f69d45b20d2a48f6b15e68
NakagamiYuta <guguru0014@gmail.com>
2019-06-26 16:11:26 +0900

    [add] テスト用の関数を実装

---
+++ testfunc.py
@@ -0,0 +1,18 @@
+class TestFailedError(Exception):
+    pass
+
+
+def test_equal(name, comp_a, comp_b):
+    if comp_a != comp_b:
+        raise TestFailedError(name)
+
+
+def test_not_equal(name, comp_a, comp_b):
+    if comp_a == comp_b:
+        raise TestFailedError(name)
+
+
+def test(name, bool_val):
+    if not bool_val:
+        raise TestFailedError(name)
+
